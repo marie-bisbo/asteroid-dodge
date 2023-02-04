753f8b9897a5161b8a4240145dc6e6721124b5ea
marie-bisbo <mariebisbo1908@gmail.com>
2023-02-04 10:58:04 +0000

    Add rocket with basic movement

---
+++ Assets/Rocket.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rigidBody;
+
+    [SerializeField]
+    private float movementSpeedVertical = 100f;
+
+    [SerializeField]
+    private float movementSpeedHorizontal = 100f;
+
+    [SerializeField]
+    private float rotationSpeed = 10f;
+
+    void FixedUpdate()
+    {
+        float moveAmountVertical = Input.GetAxis("Vertical") * movementSpeedVertical * Time.deltaTime;
+        rigidBody.AddForce(new Vector2(0, moveAmountVertical));
+
+        float moveAmountHorizontal = Input.GetAxis("Horizontal") * movementSpeedHorizontal * Time.deltaTime;
+        rigidBody.AddForce(new Vector2(moveAmountHorizontal, 0));
+
+        float rotationAmount = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        // Not working at around 0.1 because of float weirdness
+        if (Mathf.Abs(transform.rotation.z) < 0.1)
+        {
+            transform.Rotate(0, 0, -rotationAmount);
+        }
+    }
+}
